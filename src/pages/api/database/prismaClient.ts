cd36c9919de1b965911028dc909025537ba77e60
Bruno Pinheiro <bpinheiro@gavea.com>
2022-03-22 20:52:53 -0300

    fix: prisma instances

--- src/pages/api/database/prismaClient.ts
+++ src/pages/api/database/prismaClient.ts
@@ -1,4 +1,19 @@
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
+
+// export { prisma };
+
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 export { prisma };
