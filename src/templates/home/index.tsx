4f5311e23d8b4c9e28a179d027158bd3a3b7726f
Bruno Pinheiro <bpinheiro@gavea.com>
2022-03-24 23:09:20 -0300

    chore: change structure

---
+++ src/templates/home/index.tsx
@@ -0,0 +1,26 @@
+import FloatingButton from '../../components/FloatingButton';
+import ContactForm from '../../components/Forms/Contact';
+import ListContacts from '../../components/ListContacts';
+import Modal from '../../components/Modal';
+import { useModal } from '../../contexts/Modal/hooks/useModal';
+
+const HomeTemplate = () => {
+  const { isOpenModal } = useModal();
+
+  return (
+    <>
+      {isOpenModal ? (
+        <Modal>
+          <ContactForm />
+        </Modal>
+      ) : (
+        <>
+          <ListContacts />
+          <FloatingButton />
+        </>
+      )}
+    </>
+  );
+};
+
+export default HomeTemplate;
